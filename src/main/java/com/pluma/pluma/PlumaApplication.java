5d4c4b9a165f6f7b5a5be98fda92bf5a45e18b90
Stephanie <stephaniecaroline2006@gmail.com>
2025-07-31 20:58:57 -0300

    Inicialização do Projeto e Configurações do Banco de Dados com Docker

---
+++ src/main/java/com/pluma/pluma/PlumaApplication.java
@@ -0,0 +1,13 @@
+package com.pluma.pluma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlumaApplication.class, args);
+	}
+
+}
