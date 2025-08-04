31f5f6bc8a13808795f202912cfe16e1a12e30d1
Stephanie <stephaniecaroline2006@gmail.com>
2025-08-04 17:58:10 -0300

    Versão do Postgres Alterada para Menos Erros

--- src/main/java/com/pluma/pluma/modelo/livro/Livro.java
+++ src/main/java/com/pluma/pluma/modelo/livro/Livro.java
@@ -4,9 +4,6 @@ import com.pluma.pluma.util.entity.EntidadeAuditavel;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,10 +19,6 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class Livro extends EntidadeAuditavel {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
-    private Long id;
 
     @Column(nullable = false)
     private String titulo;
